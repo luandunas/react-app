c39d9978a6fdb2c6a92e470098ff8499d4cce770
Luan Santos <luandunaas@gmail.com>
2021-12-12 10:21:18 -0300

    Iniciando funções do Modal e preparando para requisitar o Endpoint de pagemento

--- src/componentes/Lista.tsx
+++ src/componentes/Lista.tsx
@@ -1,9 +1,11 @@
 import { Component } from 'react';
 import '../css/lista.css';
-import {createModal} from './Modal';
+import Modal from './Modal';
 
+//Link da API para fazer o GET dos usuários
 const api = `https://www.mocky.io/v2/5d531c4f2e0000620081ddce`;
 
+//Modelo de interface que a API vai retornar.
 interface User {
     id: string;
     name: string;
@@ -11,34 +13,53 @@ interface User {
     username: string;
 }
 
-
+//Componente lista para gerar a lista com os usuários
 class Lista extends Component<User>{
 
+     //state para fazer a funcionalidade do modal e da lista.
     state = {
+        //item para armazenar os usuários que serão retornados da API para então usar funcionalidade de array.
         users: [],
+        //item para definir se o modal está visivel ou não
+        show: false,
+        //item para definir o nome do usuário que será pago.
+        modalUsername: "",
+
+        user_id: 0,
     }
 
+    //Requisitar API quando a página terminar de carregar.
     componentDidMount = async () => {
+        //armazenado a resposta da requisição na API.
         const response = await fetch(api);
+        //definido a interface que deve conter nos dados retornado pela API.
         const obj: User = await response.json();
-
-        this.setState({users: obj});
+        //Definindo o item "users" do state para o objeto retornado pela API;
+        this.setState({ users: obj });
     }
 
-
+    //Função para criar a Lista
     createLi() {
+        //Criando um map no item users do State, passando a interface como modelo.
         return this.state.users.map((user: User, index) => {
-            console.log(user.name)
+            //console.log(user.name)
             return (
+                //Criando li para cada usuário, contendo parametro "key", "id" e um parametro customizado "data-name"
                 <li className="user" key={user.id} id={user.id} data-name={user.name}>
+                    {/* Imagem do usuário */}
                     <img className="userAvatar" src={user.img} alt="" />
+                    {/* Informações do Usuário */}
                     <div className="userInfo">
                         <div>
                             <p className="username">{user.name}</p>
                             <p className="userID">ID: {user.id} - Username: {user.username}</p>
                         </div>
+                        {/* Botão de pagamento */}
                         <div className="payButton">
-                        <button onClick={() => {createModal(user.name)}}>Pagar</button>
+                            <button onClick={() => {
+                                // Definindo visibilidade do modal e informações do usuário no objeto state.
+                                this.setState({ show: true, modalUsername: user.name, user_id: user.id })
+                            }}>Pagar</button>
                         </div>
                     </div>
                 </li>
@@ -48,9 +69,14 @@ class Lista extends Component<User>{
 
     render() {
         return (
-            <ul>
-                {this.createLi()}
-            </ul>
+            <div>
+                {/* Chamando o Modal passando props para a estrutura dele. */}
+                <Modal user_id={this.state.user_id} data-name={this.state.modalUsername} show={this.state.show} onClose={() => {this.setState({show: false})}}></Modal>
+                <ul>
+                    {/* Chamando função para criar as li. */}
+                    {this.createLi()}
+                </ul>
+            </div>
         )
     }
 }
--- src/componentes/Modal.tsx
+++ src/componentes/Modal.tsx
@@ -1,23 +1,122 @@
 import React, { Component } from "react";
+import '../css/modal.css';
 
-interface nameProps{
-    'data-name': string;
+//Definindo interface para os props que a classe vai receber.
+interface props {
+    'data-name'?: string;
+    user_id: number;
+    show: boolean;
+    onClose: Function,
 }
 
-export const createModal = (text: string)=>{
-    console.log(text);
-    return <h1>{text}</h1>
-}
+// Payload:
 
-class Modal extends Component<nameProps>{
+// interface TransactionPayload {
+//     // Card Info
+//     card_number: string;
+//     cvv: number;
+//     expiry_date: string;
+  
+//     // Destination User ID
+//     destination_user_id: number;
+  
+//     // Value of the Transaction
+//     value: number;
+//   }
+
+//cartões para exibir como pagamento.
+let cards = [
+    // valid card
+    {
+        card_number: '1111111111111111',
+        cvv: 789,
+        expiry_date: '01/18',
+    },
+    // invalid card
+    {
+        card_number: '4111111111111234',
+        cvv: 123,
+        expiry_date: '01/20',
+    },
+];
+
+//Criando classe do componente Modal.
+class Modal extends Component<props>{
 
     state = {
-        userName: "",
+        card_index: -1,
     }
 
-    render(){
+    formatCurrency(e: any){
+        const letterPattern = /[^0-9]/;
+                        if(letterPattern.test(e.key)){
+                            //console.log(e.key)
+                            e.preventDefault();
+                            return;
+                        }
+                        if(!e.currentTarget.value) return;
+                    
+                        let valor = e.currentTarget.value.toString();
+                        valor = valor.replace(/[\D]+/g, '');
+                        valor = valor.replace(/([0-9]{1})$/g, ",$1");
+                    
+                        if(valor.length >= 6){
+                            while(/([0-9]{4})[,|.]/g.test(valor)){
+                                valor = valor.replace(/([0-9]{1})$/g, ",$1");
+                                valor = valor.replace(/([0-9]{3})[,|.]/g, ".$1");
+                            }
+                        }
+                    
+                        e.currentTarget.value = valor;
+    }
+
+    payUser(id: number, valueToPay: number, card_index: number){
+        console.log(id, valueToPay, cards[card_index]);
+
+        document.getElementsByClassName("modalBody")[0].innerHTML = "<h1>Pagamento Efetuado com sucesso!</h1>"
+    }
+
+    createModal() {
+        if (!this.props.show) {
+            return null;
+        }
+
+        //console.log(this.state.card_index)
+
         return (
-            createModal(this.props["data-name"])
+            <div className="modal">
+                <div className="modalHeader">
+                    <p>Pagamento para <span className="username">{this.props["data-name"]}</span></p>
+                    <button className="closeBtn" onClick={() => { this.props.onClose() }}>Fechar</button>
+                </div>
+
+                <div className="modalBody">
+                    <input type="text" onKeyPress={(event) => {this.formatCurrency(event)}} />
+                    <select name="creditCards" id="creditCards">
+                        {
+                            cards.map((card, index) => {
+                                return (
+                                    <option data-key={index} key={index} value={card.card_number}>{`Cartão com final ${card.card_number.slice(card.card_number.length - 4)}`}</option>
+                                )
+                            })
+                        }
+                    </select>
+                    <button onClick={() =>{
+                        let valueToPay = document.getElementsByTagName('input')[0].value.replace(/[^0-9]/g, "");
+                        //console.log(Number(valueToPay.substring(0, valueToPay.length-2) + "." + valueToPay.substring(valueToPay.length-2)))
+                        let finalValue = Number(valueToPay.substring(0, valueToPay.length-2) + "." + valueToPay.substring(valueToPay.length-2));
+                        let selectElement = document.getElementById("creditCards") as HTMLSelectElement;
+                        let card_index = Number(selectElement.options[selectElement.selectedIndex].getAttribute("data-key"));
+                        this.payUser(this.props.user_id, finalValue, card_index)
+                        }}>Pagar</button>
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            this.createModal()
         )
     }
 }
